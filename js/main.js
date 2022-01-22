72811e9bcbd5080949a52bf02ecd3a646cda204f
vakhidovulugbek03 <vulugbekk@gmail.com>
2022-01-22 08:26:38 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-// let a = 0;
-// document.getElementById('modeBtn').addEventListener('click', function () {
-//   // document.getElementById('header').classList.toggle('dark');
-//   document.body.classList.toggle('dark');
-//   if (a == 0) {
-//     a++;
-//   } else {
-//     a = 0;
-//   }
-// });
+let BTN = document.getElementById("btn")
+
+document.getElementById('modeBtn').addEventListener('click', function () {
+  BTN.classList.toggle('toggl')
+});
+
+
 
 let modalBtn = document.getElementById("openBtn");
 let modal = document.getElementById("openModal")
@@ -15,7 +12,6 @@ let closeModal = document.getElementById("closeBtn")
 let modalBtn2 = document.getElementById("openBtn2");
 let modal2 = document.getElementById("openModal2")
 let closeModal2 = document.getElementById("closeBtn2")
-// let bodyPadding = document.getElementById("bodyPadding")
 
 modalBtn.addEventListener('click',function(){
   modal.style.display = "block"
@@ -33,7 +29,5 @@ closeModal2.addEventListener('click', function(){
   modal2.style.display = 'none'
 })
 
-closeBtn2.addEventListener("click" , function (e){
-  console.log(e);
-})
+
 
